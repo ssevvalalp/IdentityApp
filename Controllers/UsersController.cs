cb41771e013b32b4bd0e323f99ee35b99440fbf3
Şevval Alp <ssevvalalp@gmail.com>
2024-02-23 15:57:32 +0300

    Create Post Form

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using IdentiyApp.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IdentiyApp.Controllers
@@ -14,7 +15,7 @@ namespace IdentiyApp.Controllers
         {
             _userManager = userManager;
         }
-        
+
         public IActionResult Index()
         {
             return View(_userManager.Users);
@@ -23,6 +24,27 @@ namespace IdentiyApp.Controllers
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var newUser = new IdentityUser { UserName = model.UserName, Email = model.Email };
+               
+               IdentityResult result = await _userManager.CreateAsync(newUser, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                foreach(IdentityError err in result.Errors)
+                {
+                    ModelState.AddModelError("",err.Description);
+                }
+            }
+            return View(model);
         }
     }
 }
