54f5d9b3df95ea4e9db6fb1099533b0d261b64f2
Şevval Alp <ssevvalalp@gmail.com>
2024-02-23 12:08:59 +0300

    dotnet add package Microsoft.AspNetCore.Identity.EntityFrameworkCore --version 8.0.2
    dotnet add package Microsoft.EntityFrameworkCore.Sqlite --version 8.0.2
    
    IdentityContext
    
    were added

---
+++ Models/IdentityContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentiyApp.Models
+{
+    public class IdentityContext: IdentityDbContext<IdentityUser>
+
+    {
+        //dışarıdan(development.json) options verilecek
+        //ya da burada OnConfiguring Methodu içinde ConnectionString
+        public IdentityContext(DbContextOptions<IdentityContext>options):base(options)
+        {
+            
+        }
+    }
+}
