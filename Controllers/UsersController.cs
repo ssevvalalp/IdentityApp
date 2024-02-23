3fe8f2c62e989ed4d4b42b9b45f7aef7a3ed98a5
Şevval Alp <ssevvalalp@gmail.com>
2024-02-23 14:25:18 +0300

    UserManager > User List

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentiyApp.Controllers
+{
+    public class UsersController : Controller
+    {
+
+        //UserManager Class implementation
+
+        private UserManager<IdentityUser> _userManager;
+        public UsersController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        
+        public IActionResult Index()
+        {
+            return View(_userManager.Users);
+        }
+    }
+}
