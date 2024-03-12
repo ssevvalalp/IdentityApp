73a8131a9ec56251c4726a8040f77554af1d5366
Şevval Alp <ssevvalalp@gmail.com>
2024-03-12 21:38:48 +0300

    // if (context.Database.GetAppliedMigrations().Any()) => migration oluşturulmuş ve uygulanmış olan varsa      if (context.Database.GetPendingMigrations().Any()) //=> migration oluşturulmuş ancak uygulanmamış varsa

--- Models/IdentitySeedData.cs
+++ Models/IdentitySeedData.cs
@@ -8,24 +8,26 @@ namespace IdentiyApp.Models
         private const string adminUser = "admin";
         private const string adminPassword = "Admin_123";
 
-        //program.cs'deki app bilgisini buraya parametre olarak göndercez
+        //program.cs'deki app bilgisini(builder dosyasını) buraya parametre olarak göndercez
         public static async void IdentityTestUser(IApplicationBuilder app)
         {
-            // IdentityContext service konteyner'ından alınacak. app üzerinden bu konteynera erişilebiliyor
+            // IdentityContext servisi, service konteyner'ından alınacak. app üzerinden bu konteynera erişilebiliyor
 
             var context = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<IdentityContext>();
-            if (context.Database.GetAppliedMigrations().Any())
+
+           // if (context.Database.GetAppliedMigrations().Any()) => migration oluşturulmuş ve uygulanmış olan varsa
+                if (context.Database.GetPendingMigrations().Any()) //=> migration oluşturulmuş ancak uygulanmamış varsa
             {
-                context.Database.Migrate();//database update
+                context.Database.Migrate();//migrate = database update
             }
             //hazır servisler
 
             var userManager = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<UserManager<AppUser>>();
-            var user = await userManager.FindByNameAsync(adminUser);
+            var user = await userManager.FindByNameAsync(adminUser); //usermanager üzerinden bir user(aslında admin user) olusturma
 
             if (user == null) {
 
-                user = new AppUser
+                user = new AppUser // new IdentityUser
                 {
                     FullName = "Şevval Alp",
                     UserName = adminUser,
