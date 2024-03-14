a3fe7ede85cdaf0c0ae6087ecffa8210f414c9fa
Şevval Alp <ssevvalalp@gmail.com>
2024-03-14 11:43:34 +0300

    Role Detay Page => Users at Roles and Users Detail Page

--- Controllers/RolesController.cs
+++ Controllers/RolesController.cs
@@ -7,10 +7,11 @@ namespace IdentiyApp.Controllers
     public class RolesController : Controller
     {
         private readonly RoleManager<AppRole> _roleManager;
-        public RolesController(RoleManager<AppRole> roleManager)
+        private readonly UserManager<AppUser> _userManager;
+        public RolesController(RoleManager<AppRole> roleManager, UserManager<AppUser> userManager)
         {
             _roleManager = roleManager;
-
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -42,6 +43,20 @@ namespace IdentiyApp.Controllers
                 }
             }
             return View(model);
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+
+            if (role != null && role.Name != null)
+            {
+                ViewBag.Users = await _userManager.GetUsersInRoleAsync(role.Name);
+                    return View(role);
+  
+            }
+            return RedirectToAction("Index");
+
         }
 
     }
