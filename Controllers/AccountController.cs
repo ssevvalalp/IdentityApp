4b7f8788a8dc98c57e530368de7442c5b9e0acc4
Şevval Alp <ssevvalalp@gmail.com>
2024-03-15 11:19:32 +0300

    Cookie Authentication

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,12 +1,63 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IdentiyApp.Models;
+using IdentiyApp.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace IdentiyApp.Controllers
 {
     public class AccountController:Controller
     {
+        private UserManager<AppUser> _userManager; //<IdentityUser>
+        private RoleManager<AppRole> _roleManager;
+        private SignInManager<AppUser> _signInManager; //kullanıcı login oldugunda cookie bilgisini gönderir
+        public AccountController(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+        }
         public IActionResult Login()
         {
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                if(user != null)
+                {
+                    await _signInManager.SignOutAsync(); //kullanıcı daha önce giriş yaptıysa tarayıcısından cookie'yi sil
+                    //yeni cookie oluştur
+
+                    var result = await _signInManager.PasswordSignInAsync(user, model.Password, model.RememberMe, true); //PasswordSignInAsync özellikleri
+
+                    if(result.Succeeded) //parola doğruysa lockout ayarlarını sıfırla 
+                    {
+                        await _userManager.ResetAccessFailedCountAsync(user);
+                        await _userManager.SetLockoutEndDateAsync(user, null);
+
+                        return RedirectToAction("Index", "Home"); //succeed oldugunda
+                    }
+                    else if (result.IsLockedOut) // 5 kere yanlıs girdi hesap 5 dk kilitlendi
+                    {
+                        var lockoutDate = await _userManager.GetLockoutEndDateAsync(user);
+                        var timeleft = lockoutDate.Value - DateTime.UtcNow;
+                        ModelState.AddModelError("", $"hesabınız kitlendi, {timeleft.Minutes} dakika sonra deneyiniz");
+                    }
+                    else  {
+                        ModelState.AddModelError("", "hatalı parola");
+                    }
+                }
+                
+                else
+                {
+                    ModelState.AddModelError("", "bu email ile bir hesap bulunamadı");
+                }
+            }
+            return View(model);
+        }
     }
+
 }
