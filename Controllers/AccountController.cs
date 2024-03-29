de1329b7755b3d8c7c37e8c17287cfe1888ec5fe
Şevval Alp <ssevvalalp@gmail.com>
2024-03-29 15:24:23 +0300

    Reset Password

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -159,6 +159,45 @@ namespace IdentiyApp.Controllers
             TempData["message"] = "Epostanız gelen link ile şifrenizi yenileyebilirsiniz";
             return View();
         }
+
+        public IActionResult ResetPassword(string Id, string token)
+        {
+
+            if(Id == null || token == null)
+            {
+                return RedirectToAction("Login");
+            }
+
+            var model = new ResetPasswordModel { Token = token };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ResetPassword(ResetPasswordModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                if (user == null)
+                {
+                    TempData["message"] = "Bu mail adresiyle eşleşen kullanıcı bulunamadı.";
+                    return RedirectToAction("Login");
+                }
+                var result = await _userManager.ResetPasswordAsync(user, model.Token, model.Password);
+
+                if (result.Succeeded)
+                {
+                    TempData["message"] = "Şifreniz değiştirildi";
+                    return RedirectToAction("Login");
+                }
+
+                foreach (IdentityError err in result.Errors)
+                {
+                    ModelState.AddModelError("", err.Description);
+                }
+            }
+            return View(model);
+        }
     }
 
 }
---
+++ ViewModels/ResetPasswordModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentiyApp.ViewModels
+{
+    public class ResetPasswordModel
+    {
+
+        [Required]
+        public string Token { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage = "Parola eşleşmiyor.")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+
+    }
+}
